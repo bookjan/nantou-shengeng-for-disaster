347c1da9ba539e3f67ebab57b68eeeda14b854e6
Johnson Su <johnsonsu@iis.sinica.edu.tw>
2017-04-05 20:48:52 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+var request = require('request');
+var fs = require('fs');
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 80;
+
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res, next) {
+    request('http://alerts.ncdr.nat.gov.tw/RssAtomFeed.ashx').pipe(fs.createWriteStream('public/ncdr.xml'))
+})
+
+app.get('/ncdr', function(req, res) {
+    res.sendFile(__dirname + '/public/ncdr.xml')
+})
+
+app.get('/test', function(req, res) {
+    res.sendFile(__dirname + '/public/test.xml')
+})
+
+app.listen(port, function() {
+    console.log('Express app listening on port ' + port + '.');
+});
---
+++ public/ncdr.js
@@ -0,0 +1,55 @@
+$(function() {
+    var entrylist;
+    $.get("http://6024e5a3.ngrok.io/ncdr", function(data) {
+
+        // var xmlDoc = $.parseXML(data)
+        var $xml = $(data);
+
+        $categoryList = $xml.find("category");
+        var vals = [];
+        $("#selectCategory").append("<option>全部</option>");
+        // $("#selectCategory").append($categoryList.text());
+
+        $.each($categoryList, function() {
+            var category = $(this).attr("term");
+            if (vals.length > 0 && vals.indexOf(category) == -1) {
+                $("#selectCategory").append("<option>" + category + "</option>");
+            }
+            vals.push(category);
+        });
+
+        $entryList = $xml.find("entry");
+        $.each($entryList, function() {
+            var title = $(this).find("title").text();
+            var updated = $(this).find("updated").text();
+            var author = $(this).find("name").text();
+            var link = $(this).find("link").attr("href");
+            var summary = $(this).find("summary").text();
+            var ramdomID = "d" + Math.floor(Math.random() * 111111).toString();
+            var content = "發布單位: " + author + "<br/>" + "" + title + "<br/>" + "" + updated + "<br/>" + "" + link + "<br/>" + "<div id=" + ramdomID + "></div><br/>" + "" + summary + "<br/>" + "<hr/>"
+            $("#DivContent").append(content);
+        });
+
+        entrylist = $entryList;
+
+    });
+
+    $('select').on('change', function() {
+        var selectedItem = this.value;
+        $("#DivContent").empty();
+        $.each(entrylist, function() {
+            var title = $(this).find("title").text();
+            if (title == selectedItem) {
+                var updated = $(this).find("updated").text();
+                var author = $(this).find("name").text();
+                var link = $(this).find("link").attr("href");
+                var summary = $(this).find("summary").text();
+                var ramdomID = "d" + Math.floor(Math.random() * 111111).toString();
+                var content = "發布單位: " + author + "<br/>" + "" + title + "<br/>" + "" + updated + "<br/>" + "" + link + "<br/>" + "<div id=" + ramdomID + "></div><br/>" + "" + summary + "<br/>" + "<hr/>"
+                $("#DivContent").append(content);
+            }
+        });
+    })
+
+
+});
